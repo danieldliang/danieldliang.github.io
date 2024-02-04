0548dfa5b44501aaf97b3a34b1a790d30f2858b8
danieldliang <134559032+danieldliang@users.noreply.github.com>
2024-02-03 19:57:22 -0500

    Update segtree.cpp

--- stuff/usaco/algos/segtree.cpp
+++ stuff/usaco/algos/segtree.cpp
@@ -11,12 +11,10 @@ int tree[2 * N];
 void build(vector<int> arr) 
 { 
 	// insert leaf nodes in tree 
-	for (int i=0; i<n; i++)	 
-		tree[n+i] = arr[i]; 
+	for (int i = 0; i < n; ++i) tree[n+i] = arr[i]; 
 	
 	// build the tree by calculating parents 
-	for (int i = n - 1; i > 0; --i)
-    tree[i] = tree[i<<1] + tree[i<<1 | 1];
+	for (int i = n - 1; i > 0; --i) tree[i] = tree[i<<1] + tree[i<<1 | 1];
 }
 
 // function to update a tree node 
@@ -33,18 +31,14 @@ void updateTreeNode(int p, int value)
 
 // function to get sum on interval [l, r) 
 int query(int l, int r) 
-{ 
+{
 	int res = 0; 
 	
 	// loop to find the sum in the range 
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) 
-  { 
-      if (l&1)  
-          res += tree[l++]; 
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) { 
+		if (l&1) res += tree[l++]; 
+		if (r&1) res += tree[--r]; 
+	} 
     
-      if (r&1)  
-          res += tree[--r]; 
-  } 
-    
-  return res; 
+	return res; 
 } 
