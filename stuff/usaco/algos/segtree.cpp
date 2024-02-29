d3a4e5f9ddb6e13a22b07b6def69e76ee3c32a83
danieldliang <134559032+danieldliang@users.noreply.github.com>
2024-02-28 21:58:33 -0500

    Update segtree.cpp

--- stuff/usaco/algos/segtree.cpp
+++ stuff/usaco/algos/segtree.cpp
@@ -1,42 +1,38 @@
-// Took and edited off of GFGs
-const int N = 200002; // 2 * max array size + 2
+#include <bits/stdc++.h>
+using namespace std;
 
-int n;
+using ll = long long;
 
-// Max size of tree 
-int tree[2 * N]; 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-// function to build the tree 
-void build(vector<int> arr) 
-{ 
-	// insert leaf nodes in tree 
-	for (int i = 0; i < n; ++i) tree[n + i] = arr[i]; 
-	
-	// build the tree by calculating parents 
-	for (int i = n - 1; i > 0; --i) tree[i] = tree[i << 1] + tree[i << 1 | 1];
+    ll n, m;
+    cin >> n >> m;
+
+    ll segtree[2 * n + 20];
+    for (ll i = 0; i < n; ++i) cin >> segtree[n + i];
+    for (ll i = n - 1; i > 0; --i) segtree[i] = segtree[i << 1] + segtree[i << 1|1];
+
+    ll choice;
+    ll f, s;
+    ll res;
+    for (ll i = 0; i < m; ++i) {
+        cin >> choice;
+        if (choice == 1) {
+            cin >> f >> s;
+            f--;
+            segtree[n + f] = s;
+            for (ll j = n + f; j > 1; j >>= 1)
+                segtree[j >> 1] = segtree[j] + segtree[j ^ 1];
+        } else {
+            cin >> f >> s; f--;
+            res = 0;
+            for (f += n, s += n; f < s; f >>= 1, s >>= 1) {
+                if (f & 1) res += segtree[f++];
+                if (s & 1) res += segtree[--s];
+            }
+            cout << res << endl;
+        }
+    }
 }
-
-// function to update a tree node 
-void updateTreeNode(int p, int value)  
-{  
-    // set value at position p 
-    tree[p + n] = value;
-	
-    // move upward and update parents 
-    for (int i = n + p; i > 1; i >>= 1) 
-        tree[i >> 1] = tree[i] + tree[i ^ 1]; 
-}
-
-// function to get sum on interval [l, r) 
-int query(int l, int r) 
-{
-	int res = 0;
-	
-	// loop to find the sum in the range 
-	for (l += n, r += n; l < r; l >>= 1, r >>= 1) { 
-		if (l & 1) res += tree[l++]; 
-		if (r & 1) res += tree[--r]; 
-	} 
-    
-	return res; 
-} 
