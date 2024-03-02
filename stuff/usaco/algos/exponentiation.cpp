124606d870e8859a69b6b6465f2890990e7bd1ca
danieldliang <134559032+danieldliang@users.noreply.github.com>
2024-03-01 21:31:29 -0500

    Create exponentiation.cpp

---
+++ stuff/usaco/algos/exponentiation.cpp
@@ -0,0 +1,28 @@
+// USACO guide solution to https://cses.fi/problemset/task/1095
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+ll exp(ll x, ll n, ll m) {
+	assert(n >= 0);
+	x %= m;  // note: m * m must be less than 2^63 to avoid ll overflow
+	ll res = 1;
+	while (n > 0) {
+		if (n % 2 == 1) { res = res * x % m; }
+		x = x * x % m;
+		n /= 2;
+	}
+	return res;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << exp(a, b, 1e9 + 7) << "\n";
+	}
+}
